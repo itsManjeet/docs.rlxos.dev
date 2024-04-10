dd98568474906d580a406364299d611a534b2e47
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-10 15:18:06 +0530

    updated documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Handbook</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/itsmanjeet/rlxos',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/itsmanjeet/docs.rlxos.dev',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'rlxos Handbook',
   },
 }
 
