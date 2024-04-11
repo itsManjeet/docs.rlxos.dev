03a278de0f8d91062d38c6dd3cfd0953910c8795
itsmanjeet <itsmanjeet1998@gmail.com>
2024-04-11 17:25:36 +0530

    updated webhooks

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   output: 'export',
+  assetPrefix: "./",
+  images: {
+    unoptimized: true
+  }
 })
 
 module.exports = withNextra()
